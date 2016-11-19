37ddbe44283b12d7452c0becf9eaffcfe4bfb87a
Egor_Leontev <egor.leontev@kronos.com>
2016-11-19 15:48:48 +0300

    #27 figure type J was implemented

--- app/constants/figure.constants.ts
+++ app/constants/figure.constants.ts
@@ -153,10 +153,61 @@ const FIGURE_TYPE_O = {
     }
 };
 
+const FIGURE_TYPE_J = {
+    views: {
+        STATE_0: [
+            {x: -1, y:  0},
+            {x:  0, y: -2},
+            {x:  0, y: -1},
+            {x:  0, y:  0}
+        ],
+        STATE_1: [
+            {x: -1, y: -1},
+            {x:  0, y: -1},
+            {x:  1, y: -1},
+            {x:  1, y:  0}
+        ],
+        STATE_2: [
+            {x: -1, y: -2},
+            {x: -1, y: -1},
+            {x: -1, y:  0},
+            {x:  0, y: -2}
+        ],
+        STATE_3: [
+            {x: -1, y: -1},
+            {x: -1, y:  0},
+            {x:  0, y:  0},
+            {x:  1, y:  0}
+        ]
+    },
+    cordsToCheck: {
+        STATE_0: {
+            nextStep: [0, 3],
+            moveRight: [1, 2, 3],
+            moveLeft: [0, 1, 2]
+        },
+        STATE_1: {
+            nextStep: [0, 1, 3],
+            moveRight: [2, 3],
+            moveLeft: [0, 3]
+        },
+        STATE_2: {
+            nextStep: [2, 3],
+            moveRight: [1, 2, 3],
+            moveLeft: [0, 1, 2]
+        },
+        STATE_3: {
+            nextStep: [1, 2, 3],
+            moveRight: [0, 3],
+            moveLeft: [0, 1]
+        }
+    }
+};
+
 export const CHECK_NEXT = `nextStep`;
 export const CHECK_LEFT = `moveLeft`;
 export const CHECK_RIGHT = `moveRight`;
 export const CHECK_BOTTOM = `CHECK_BOTTOM`;
-export const LIST_OF_FIGURES = [FIGURE_TYPE_I, FIGURE_TYPE_T, FIGURE_TYPE_O];
-// export const LIST_OF_FIGURES = [FIGURE_TYPE_O];
+export const LIST_OF_FIGURES = [FIGURE_TYPE_I, FIGURE_TYPE_T, FIGURE_TYPE_O, FIGURE_TYPE_J];
+// export const LIST_OF_FIGURES = [FIGURE_TYPE_J];
 export const LIST_VIEWS = [`STATE_0`, `STATE_1`, `STATE_2`, `STATE_3`];
