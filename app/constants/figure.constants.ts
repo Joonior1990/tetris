95069db89faee86608ec7cee88aa04e0c410dcbe
Egor_Leontev <egor.leontev@kronos.com>
2016-11-20 17:00:27 +0300

    #32 figure type Z was implemented

--- app/constants/figure.constants.ts
+++ app/constants/figure.constants.ts
@@ -306,62 +306,60 @@ const FIGURE_TYPE_S = {
     }
 };
 
-// const FIGURE_TYPE_Z = {
-//     views: {
-//         STATE_0: [
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: }
-//         ],
-//         STATE_1: [
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: }
-//         ],
-//         STATE_2: [
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: }
-//         ],
-//         STATE_3: [
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: },
-//             {x: , y: }
-//         ]
-//     },
-//     cordsToCheck: {
-//         STATE_0: {
-//             nextStep: [],
-//             moveRight: [],
-//             moveLeft: []
-//         },
-//         STATE_1: {
-//             nextStep: [],
-//             moveRight: [],
-//             moveLeft: []
-//         },
-//         STATE_2: {
-//             nextStep: [],
-//             moveRight: [],
-//             moveLeft: []
-//         },
-//         STATE_3: {
-//             nextStep: [],
-//             moveRight: [],
-//             moveLeft: []
-//         }
-//     }
-// };
-
+const FIGURE_TYPE_Z = {
+    views: {
+        STATE_0: [
+            {x: -1, y: -1},
+            {x:  0, y: -1},
+            {x:  0, y:  0},
+            {x:  1, y:  0}
+        ],
+        STATE_1: [
+            {x:  0, y:  0},
+            {x:  0, y: -1},
+            {x:  1, y: -1},
+            {x:  1, y: -2}
+        ],
+        STATE_2: [
+            {x: -1, y: -1},
+            {x:  0, y: -1},
+            {x:  0, y:  0},
+            {x:  1, y:  0}
+        ],
+        STATE_3: [
+            {x:  0, y:  0},
+            {x:  0, y: -1},
+            {x:  1, y: -1},
+            {x:  1, y: -2}
+        ]
+    },
+    cordsToCheck: {
+        STATE_0: {
+            nextStep: [0, 2, 3],
+            moveRight: [1, 3],
+            moveLeft: [0, 2]
+        },
+        STATE_1: {
+            nextStep: [0, 2],
+            moveRight: [0, 2, 3],
+            moveLeft: [0, 1, 3]
+        },
+        STATE_2: {
+            nextStep: [0, 2, 3],
+            moveRight: [1, 3],
+            moveLeft: [0, 2]
+        },
+        STATE_3: {
+            nextStep: [0, 2],
+            moveRight: [0, 2, 3],
+            moveLeft: [0, 1, 3]
+        }
+    }
+};
 
 export const CHECK_NEXT = `nextStep`;
 export const CHECK_LEFT = `moveLeft`;
 export const CHECK_RIGHT = `moveRight`;
 export const CHECK_BOTTOM = `CHECK_BOTTOM`;
-// export const LIST_OF_FIGURES = [FIGURE_TYPE_I, FIGURE_TYPE_T, FIGURE_TYPE_O, FIGURE_TYPE_J, FIGURE_TYPE_L, FIGURE_TYPE_S];
-export const LIST_OF_FIGURES = [FIGURE_TYPE_S];
+export const LIST_OF_FIGURES = [FIGURE_TYPE_I, FIGURE_TYPE_T, FIGURE_TYPE_O, FIGURE_TYPE_J, FIGURE_TYPE_L, FIGURE_TYPE_S, FIGURE_TYPE_Z];
 export const LIST_VIEWS = [`STATE_0`, `STATE_1`, `STATE_2`, `STATE_3`];
